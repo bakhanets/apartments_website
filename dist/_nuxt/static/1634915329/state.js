5a6926603b85331d1205efaf17e00d02e16321a6
AlexBessmelcev <alexbessmelcev@gmail.com>
2021-10-22 22:29:25 +0700

    add title logov2

--- dist/_nuxt/static/1634915329/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634915329",layout:"default",error:a,state:{modals:{isShow:false,ddValues:{},currentModalKey:"",options:{},showWelcomeModal:b,modals:{}}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true,"\u002F"));
---
+++ dist/_nuxt/static/1634916551/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634916551",layout:"default",error:a,state:{modals:{isShow:false,ddValues:{},currentModalKey:"",options:{},showWelcomeModal:b,modals:{}}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true,"\u002F"));
